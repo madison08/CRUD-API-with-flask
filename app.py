91db666fc9b0c9d7fdbbf411c8307f61fa7c1851
madison <madisonamoa@gmail.com>
2021-07-20 12:48:29 +0000

    add get,put and delete

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask,jsonify
 import datetime
+from flask.wrappers import Request
 from flask_restful import Resource,Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from werkzeug.exceptions import abort
+from werkzeug.wrappers import request
 
 
 
@@ -35,6 +38,59 @@ class Todo(db.Model):
             'create_at': self.create_at
         }
 
+
+class TodoId(Resource):
+
+    def get(self,id):
+
+        todo = Todo.query.filter_by(id=id).first()
+
+        if todo:
+            return todo.json()
+        else:
+            return {"note": None},404
+
+    def put(self,id):
+
+        todo = Todo.query.filter_by(id=id).first()
+        
+        request_data = request.form['task']
+        todo.task = request_data
+
+        db.session.add(todo)
+        db.session.commit()
+
+        if not todo:
+            abort(404, "todo not found")
+        
+        return todo.json
+
+    def delete(self,id):
+
+        todo = Todo.query.filter_by(id=id).first()
+
+        db.session.delete(todo)
+        db.session.commit()
+
+        if todo:
+            return todo.json()
+        else:
+            return {'note': 'not found'},404
+
+class Todo(Resource):
+
+    def post(self):
+
+        todoname = Request.form['task']
+
+        todo = Todo(task=todoname)
+        db.session.add(todo)
+        db.session.commit()
+
+        return todo.json()
+
+
+api.add_resource(TodoId,'api/v1/<int:id>')
 
 
 
