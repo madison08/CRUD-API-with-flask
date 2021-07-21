55139dcdd0672e86a1cd6847acee763810dfdc26
madison <madisonamoa@gmail.com>
2021-07-21 11:00:29 +0000

    resolve bug on post, review put,delete

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask,jsonify
-import datetime
-from flask.wrappers import Request
-from flask_restful import Resource,Api
+from flask import Flask,jsonify,Request
+from datetime import datetime
+import json
+from flask.wrappers import Response
+from flask_restful import Resource,Api,reqparse
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from werkzeug.exceptions import abort
@@ -14,9 +15,22 @@ app = Flask(__name__)
 # Our configuration
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 Migrate(app,db)
 api = Api(app)
+
+
+# response structure
+
+
+# ressource_fields = {
+
+#     'task': fields.String,
+# }
+
+parser = reqparse.RequestParser()
+parser.add_argument('task')
 
 
 # Our model
@@ -26,17 +40,21 @@ class Todo(db.Model):
 
     id = db.Column(db.Integer,primary_key=True)
     task = db.Column(db.String(150))
-    create_at = db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
+    create_at = db.Column(db.DateTime,nullable=False,default=datetime.utcnow())
 
     def __init__(self,task):
         self.task = task
+
+
 
     def json(self):
         return {
             'id': self.id,
             'task': self.task,
-            'create_at': self.create_at
+            'create_at': str(self.create_at)
         }
+
+
 
 
 class TodoId(Resource):
@@ -54,43 +72,65 @@ class TodoId(Resource):
 
         todo = Todo.query.filter_by(id=id).first()
         
-        request_data = request.form['task']
-        todo.task = request_data
+        if not todo:
+            abort(404, "todo not found")
+
+        args = parser.parse_args()
+        _task= args["task"]
+
+        todo.task = _task
 
         db.session.add(todo)
         db.session.commit()
 
-        if not todo:
-            abort(404, "todo not found")
         
-        return todo.json
+        return todo.json()
 
     def delete(self,id):
 
         todo = Todo.query.filter_by(id=id).first()
 
-        db.session.delete(todo)
-        db.session.commit()
+        
 
         if todo:
+
+            db.session.delete(todo)
+            db.session.commit()
+
             return todo.json()
         else:
             return {'note': 'not found'},404
 
-class Todo(Resource):
+class TodoA(Resource):
+    # Ressource for add and list todos
+
+    def get(self):
+        todos = Todo.query.all()
+
+        return [todo.json() for todo in todos]
+
 
     def post(self):
 
-        todoname = Request.form['task']
+        args = parser.parse_args()
+        _task= args["task"]
 
-        todo = Todo(task=todoname)
-        db.session.add(todo)
+        new_task = Todo(task=_task)
+        db.session.add(new_task)
         db.session.commit()
 
-        return todo.json()
+        # print('^^^^^^^^^^^^^^^^^')
+
+        return new_task.json()
 
 
-api.add_resource(TodoId,'api/v1/<int:id>')
+
+        # return {'data': todo.json()}
+
+    
+
+api.add_resource(TodoId,'/api/v1/todo/<int:id>')
+api.add_resource(TodoA,'/api/v1/todo')
 
 
 
